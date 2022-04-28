8e17c6485fad4feed5e6bdd637b3a2586cbb8e8e
semsmirn <semen.smirnov@opencascade.com>
2022-04-28 09:09:12 +0300

    Initial commit

---
+++ SocketTcpClient/SocketTcpClient/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace SocketTcpClient
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        int aPort = 8005;
+        string anAddress = "127.0.0.1";
+        IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(anAddress), aPort);
+
+        Socket aSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        aSocket.Connect(ipPoint);
+
+        string aMessage;
+        StringBuilder builder = new StringBuilder("");
+
+        while (true)
+        {
+          Console.Write("Enter a message:");
+          aMessage = Console.ReadLine();
+          if (aMessage == "") continue;
+
+          byte[] aData = Encoding.Unicode.GetBytes(aMessage);
+
+          aSocket.Send(aData);
+          aData = new byte[256];
+          int aBytes = 0;
+
+          builder = new StringBuilder("");
+
+          do
+          {
+            aBytes = aSocket.Receive(aData, aData.Length, 0);
+            builder.Append(Encoding.UTF8.GetString(aData, 0, aBytes));
+          }
+          while (aSocket.Available > 0);
+          Console.WriteLine(builder.ToString());
+
+
+          if (aMessage == "shutdown")
+          {
+            aSocket.Shutdown(SocketShutdown.Both);
+            aSocket.Close();
+            break;
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+  }
+}
---
+++ SocketTcpServer/SocketTcpServer/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace SocketTcpServer
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int aPort = 8005;
+      string aHost = Dns.GetHostName();
+      Console.WriteLine("Host: " + aHost);
+
+      IPAddress[] IPs;
+      IPs = Dns.GetHostAddresses(aHost);
+      foreach (IPAddress ip1 in IPs)
+      {
+        Console.WriteLine(ip1);
+      }
+
+      IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), aPort);
+
+      Socket aListenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+      try
+      {
+        aListenSocket.Bind(ipPoint);
+        aListenSocket.Listen(10);
+
+        StringBuilder aBuilder = new StringBuilder("");
+        int aBytes = 0;
+        int aNumberBytes = 0;
+        byte[] aData = new byte[255];
+
+        Socket aSocketHandler = aListenSocket.Accept();
+
+        while (true)
+        {
+          if (!aSocketHandler.Connected)
+          {
+            aSocketHandler = aListenSocket.Accept();
+          }
+
+
+          aBuilder = new StringBuilder("");
+
+          do
+          {
+            aBytes = aSocketHandler.Receive(aData);
+            aBuilder.Append(Encoding.Unicode.GetString(aData, 0, aBytes));
+            aNumberBytes += aBytes;
+          }
+          while (aSocketHandler.Available > 0);
+
+          Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + aBuilder.ToString());
+          Console.WriteLine(aNumberBytes + "bytes\n");
+
+          byte[] anArrayBytes = Encoding.UTF8.GetBytes("Server response");
+
+          aSocketHandler.Send(anArrayBytes);
+
+          if (aBuilder.ToString() == "shutdown")
+          {
+            aSocketHandler.Shutdown(SocketShutdown.Both);
+            aSocketHandler.Close();
+          }
+        }
+
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+  }
+}
